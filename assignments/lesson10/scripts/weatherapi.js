7e3ddd78e548fe0f38936bf1138b5c71974355ae
lynndar.github.io <rob85006@byui.edu>
2019-04-13 23:00:00 -0800

    comit

--- assignments/lesson10/scripts/weatherapi.js
+++ assignments/lesson10/scripts/weatherapi.js
@@ -1,18 +1,12 @@
-let weatherRequest = new XMLHttpRequest();
-let apiURLstring = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=cd3872cd815886366b88e86f315579bb";
-weatherRequest.open('Get', apiURLstring, true);
+var weatherRequest = new XMLHttpRequest();
+var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=59b1ff61506312611db0e403529362fb';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.responseType = 'text';
 weatherRequest.send();
 
-weatherRequest.onload = function() {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+weatherRequest.onload = function () {
+    var weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('current-temp').innerHTML = weatherData.list[0].main.temp;
-
-    let icon = "https://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
-    let desc = weatherData.list[0].weather[0].description;
-
-    document.getElementById('cc-img').setAttribute('src', icon);
-    document.getElementById('cc-img').setAttribute('alt', desc);
-
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
 }
